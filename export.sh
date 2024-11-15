7ee8f729cf6dfa54adcd2cf0b11dd9109d4e2166
johan cho <johan-cho@outlook.com>
2024-11-15 16:38:28 -0500

    ...

--- export.sh
+++ export.sh
@@ -9,7 +9,8 @@ fi
 
 source .venv/bin/activate
 black --line-length 80 "$1.ipynb"
-jupyter nbconvert --to webpdf "$1.ipynb" --allow-chromium-download
+jupyter nbconvert --to qtpdf "$1.ipynb"
+mkdir -p exports
 mv "$1.pdf" exports/
 echo "Exported $1.ipynb to exports/$1.pdf"
 kde-open "exports/$1.pdf"
